0107722a914babbefda16afc2a9f27c944cb1f78
shinis(ys) <actionshin@gmail.com>
2024-10-30 10:39:33 +0900

    프로젝트 구성

---
+++ src/test/java/com/mysite/sbbshopapi/SbbShopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbShopapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
